35c045693ccf91d62da979783e849306893bf553
Phill Morton <me@phillmorton.co.uk>
2020-05-20 13:58:34 +0100

    added UI handling

--- RML.Utility/MainWindow.Designer.cs
+++ RML.Utility/MainWindow.Designer.cs
@@ -29,15 +29,21 @@
         private void InitializeComponent()
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.e = new System.Windows.Forms.Label();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.lblDatabase = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
             this.lblAuthType = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnStartProcess = new System.Windows.Forms.Button();
             this.txtServer = new System.Windows.Forms.TextBox();
             this.lblServer = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboAuthType = new System.Windows.Forms.ComboBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.textBox3 = new System.Windows.Forms.TextBox();
@@ -63,15 +69,21 @@
             // 
             // splitContainer1.Panel1
             // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnBrowse);
+            this.splitContainer1.Panel1.Controls.Add(this.txtFile);
             this.splitContainer1.Panel1.Controls.Add(this.label1);
-            this.splitContainer1.Panel1.Controls.Add(this.textBox2);
-            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
-            this.splitContainer1.Panel1.Controls.Add(this.e);
+            this.splitContainer1.Panel1.Controls.Add(this.btnHelp);
+            this.splitContainer1.Panel1.Controls.Add(this.txtDatabase);
+            this.splitContainer1.Panel1.Controls.Add(this.lblDatabase);
+            this.splitContainer1.Panel1.Controls.Add(this.lblPassword);
+            this.splitContainer1.Panel1.Controls.Add(this.txtUsername);
+            this.splitContainer1.Panel1.Controls.Add(this.txtPassword);
+            this.splitContainer1.Panel1.Controls.Add(this.lblUsername);
             this.splitContainer1.Panel1.Controls.Add(this.lblAuthType);
-            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.btnStartProcess);
             this.splitContainer1.Panel1.Controls.Add(this.txtServer);
             this.splitContainer1.Panel1.Controls.Add(this.lblServer);
-            this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.comboAuthType);
             // 
             // splitContainer1.Panel2
             // 
@@ -81,80 +93,134 @@
             this.splitContainer1.TabIndex = 0;
             this.splitContainer1.Text = "splitContainer1";
             // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(310, 241);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 11;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(162, 212);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(223, 23);
+            this.txtFile.TabIndex = 10;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 183);
+            this.label1.Location = new System.Drawing.Point(27, 212);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Server";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
             // 
-            // textBox2
+            // btnHelp
             // 
-            this.textBox2.Location = new System.Drawing.Point(180, 154);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(140, 23);
-            this.textBox2.TabIndex = 2;
+            this.btnHelp.Location = new System.Drawing.Point(162, 288);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(75, 23);
+            this.btnHelp.TabIndex = 8;
+            this.btnHelp.Text = "Help";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
             // 
-            // textBox1
+            // txtDatabase
             // 
-            this.textBox1.Location = new System.Drawing.Point(180, 183);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(140, 23);
-            this.textBox1.TabIndex = 2;
+            this.txtDatabase.Location = new System.Drawing.Point(162, 49);
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(223, 23);
+            this.txtDatabase.TabIndex = 2;
             // 
-            // e
+            // lblDatabase
             // 
-            this.e.AutoSize = true;
-            this.e.Location = new System.Drawing.Point(45, 154);
-            this.e.Name = "e";
-            this.e.Size = new System.Drawing.Size(39, 15);
-            this.e.TabIndex = 1;
-            this.e.Text = "Server";
+            this.lblDatabase.AutoSize = true;
+            this.lblDatabase.Location = new System.Drawing.Point(27, 50);
+            this.lblDatabase.Name = "lblDatabase";
+            this.lblDatabase.Size = new System.Drawing.Size(55, 15);
+            this.lblDatabase.TabIndex = 1;
+            this.lblDatabase.Text = "Database";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(27, 141);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(57, 15);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(162, 112);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(223, 23);
+            this.txtUsername.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(162, 141);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(223, 23);
+            this.txtPassword.TabIndex = 5;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(27, 112);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(60, 15);
+            this.lblUsername.TabIndex = 1;
+            this.lblUsername.Text = "Username";
             // 
             // lblAuthType
             // 
             this.lblAuthType.AutoSize = true;
-            this.lblAuthType.Location = new System.Drawing.Point(45, 120);
+            this.lblAuthType.Location = new System.Drawing.Point(27, 78);
             this.lblAuthType.Name = "lblAuthType";
             this.lblAuthType.Size = new System.Drawing.Size(86, 15);
             this.lblAuthType.TabIndex = 1;
             this.lblAuthType.Text = "Authentication";
             // 
-            // button1
+            // btnStartProcess
             // 
-            this.button1.Location = new System.Drawing.Point(245, 256);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnStartProcess.Location = new System.Drawing.Point(263, 288);
+            this.btnStartProcess.Name = "btnStartProcess";
+            this.btnStartProcess.Size = new System.Drawing.Size(122, 23);
+            this.btnStartProcess.TabIndex = 7;
+            this.btnStartProcess.Text = "Run RML ReadTrace";
+            this.btnStartProcess.UseVisualStyleBackColor = true;
+            this.btnStartProcess.Click += new System.EventHandler(this.btnStartProcess_Click);
             // 
             // txtServer
             // 
-            this.txtServer.Location = new System.Drawing.Point(180, 87);
+            this.txtServer.Location = new System.Drawing.Point(162, 18);
             this.txtServer.Name = "txtServer";
-            this.txtServer.Size = new System.Drawing.Size(140, 23);
-            this.txtServer.TabIndex = 2;
+            this.txtServer.Size = new System.Drawing.Size(223, 23);
+            this.txtServer.TabIndex = 1;
+            this.txtServer.Text = "localhost";
             // 
             // lblServer
             // 
             this.lblServer.AutoSize = true;
-            this.lblServer.Location = new System.Drawing.Point(45, 88);
+            this.lblServer.Location = new System.Drawing.Point(27, 19);
             this.lblServer.Name = "lblServer";
             this.lblServer.Size = new System.Drawing.Size(39, 15);
             this.lblServer.TabIndex = 1;
             this.lblServer.Text = "Server";
             // 
-            // comboBox1
+            // comboAuthType
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(180, 120);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(140, 23);
-            this.comboBox1.TabIndex = 0;
+            this.comboAuthType.FormattingEnabled = true;
+            this.comboAuthType.Location = new System.Drawing.Point(162, 78);
+            this.comboAuthType.Name = "comboAuthType";
+            this.comboAuthType.Size = new System.Drawing.Size(223, 23);
+            this.comboAuthType.TabIndex = 3;
             // 
             // listBox1
             // 
@@ -170,6 +236,7 @@
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "RML TraceFiles|*.xel;*.trc";
             // 
             // textBox3
             // 
@@ -258,16 +325,16 @@
         #endregion
 
         private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label lblServer;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboAuthType;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnStartProcess;
         private System.Windows.Forms.TextBox txtServer;
         private System.Windows.Forms.Label lblAuthType;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label e;
+        private System.Windows.Forms.Label lblUsername;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
@@ -276,6 +343,13 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.Label lblDatabase;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFile;
         private System.Windows.Forms.Label label1;
     }
 }
---
+++ RML.Utility/Models/ComboBoxItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace RML.Utility.Models
+{
+    public class ComboBoxItem
+    {
+        public string Text { get; set; }
+        public bool Value { get; set; }
+         
+    }
+
+
+
+    public class AuthTypeListItems : BindingList<ComboBoxItem>
+    {
+        public AuthTypeListItems()
+        {
+            base.Add(new ComboBoxItem { Text = "Windows Authentication", Value = true });
+            base.Add(new ComboBoxItem { Text = "SQL Server Authentication", Value = false });
+        }
+    }
+}
